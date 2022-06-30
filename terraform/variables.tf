1ffc051781002998a0681f6e4ce5176c83d34a2b
Amarpreet Singh <solamarpreet@gmail.com>
2022-06-30 20:18:17 +0530

    Basic working skeleton main.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,50 @@
+variable "compartment_id" {
+  description = "OCI Compartment ID"
+  type        = string
+}
+
+variable "fingerprint" {
+  description = "The fingerprint of the key to use for signing"
+  type        = string
+}
+
+variable "private_key_path" {
+  description = "The path to the private key to use for signing"
+  type        = string
+}
+
+variable "region" {
+  description = "The region to connect to. Default: eu-frankfurt-1"
+  type        = string
+  default     = "eu-frankfurt-1"
+}
+
+variable "tenancy_ocid" {
+  description = "The tenancy OCID."
+  type        = string
+}
+
+variable "user_ocid" {
+  description = "The user OCID."
+  type        = string
+}
+
+variable "ssh_authorized_keys" {
+  description = "List of authorized SSH keys"
+  type        = string
+}
+
+variable "availability_domain" {
+  description = "Availability domain to provision resources"
+  type        = string
+}
+
+variable "subnet_id" {
+  description = "OCID of the subnet"
+  type        = string
+}
+
+variable "source_image_id" {
+  description = "OCID of the image"
+  type        = string
+}
